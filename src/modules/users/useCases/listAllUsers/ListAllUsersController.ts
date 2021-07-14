18b4bd31d9b15306f2b3d3fd2ab3a3dc87cf5ad8
Eduardo Bravo <eduardobravopires@gmail.com>
2021-07-14 13:50:16 -0300

    finished challenge

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,24 @@
+/* eslint-disable prettier/prettier */
 import { Request, Response } from "express";
+import { IncomingHttpHeaders } from "http";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+type IRequest = IncomingHttpHeaders & {
+  user_id: string;
+};
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers as IRequest;
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({error: err})
+    }
   }
 }
 
